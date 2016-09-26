fc2f154fe084069d823f2319cbe79693dd4e3205
www-data <www-data@www.porsche.lv>
2016-09-26 20:12:53 +0300

    f

--- system/library/language.php
+++ system/library/language.php
@@ -11,29 +11,29 @@ class Language {
 	public function get($key) {
 		return (isset($this->data[$key]) ? $this->data[$key] : $key);
 	}
-	
+
 	public function set($key, $value) {
 		$this->data[$key] = $value;
 	}
-	
+
 	// Please dont use the below function i'm thinking getting rid of it.
 	public function all() {
 		return $this->data;
 	}
-	
+
 	// Please dont use the below function i'm thinking getting rid of it.
 	public function merge(&$data) {
 		array_merge($this->data, $data);
 	}
-			
+
 	public function load($filename, &$data = array()) {
 		$_ = array();
 
 		$file = DIR_LANGUAGE . 'english/' . $filename . '.php';
-		
+
 		// Compatibility code for old extension folders
 		$old_file = DIR_LANGUAGE . 'english/' . str_replace('extension/', '', $filename) . '.php';
-		
+
 		if (is_file($file)) {
 			require($file);
 		} elseif (is_file($old_file)) {
@@ -44,7 +44,7 @@ class Language {
 
 		// Compatibility code for old extension folders
 		$old_file = DIR_LANGUAGE . $this->default . '/' . str_replace('extension/', '', $filename) . '.php';
-		
+
 		if (is_file($file)) {
 			require($file);
 		} elseif (is_file($old_file)) {
@@ -55,7 +55,7 @@ class Language {
 
 		// Compatibility code for old extension folders
 		$old_file = DIR_LANGUAGE . $this->directory . '/' . str_replace('extension/', '', $filename) . '.php';
-		
+
 		if (is_file($file)) {
 			require($file);
 		} elseif (is_file($old_file)) {
@@ -66,4 +66,5 @@ class Language {
 
 		return $this->data;
 	}
+
 }
