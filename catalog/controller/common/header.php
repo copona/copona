c8a27a634e9bcb2781e4a999d60beea101a8fe68
www-data <www-data@www.porsche.lv>
2016-09-26 17:16:00 +0300

    formatting

--- catalog/controller/common/header.php
+++ catalog/controller/common/header.php
@@ -1,5 +1,6 @@
 <?php
 class ControllerCommonHeader extends Controller {
+
 	public function index() {
 		// Analytics
 		$this->load->model('extension/extension');
@@ -103,22 +104,22 @@ class ControllerCommonHeader extends Controller {
 
 				foreach ($children as $child) {
 					$filter_data = array(
-						'filter_category_id'  => $child['category_id'],
-						'filter_sub_category' => true
+						'filter_category_id'	 => $child['category_id'],
+						'filter_sub_category'	 => true
 					);
 
 					$children_data[] = array(
-						'name'  => $child['name'] . ($this->config->get('config_product_count') ? ' (' . $this->model_catalog_product->getTotalProducts($filter_data) . ')' : ''),
-						'href'  => $this->url->link('product/category', 'path=' . $category['category_id'] . '_' . $child['category_id'])
+						'name' => $child['name'] . ($this->config->get('config_product_count') ? ' (' . $this->model_catalog_product->getTotalProducts($filter_data) . ')' : ''),
+						'href' => $this->url->link('product/category', 'path=' . $category['category_id'] . '_' . $child['category_id'])
 					);
 				}
 
 				// Level 1
 				$data['categories'][] = array(
-					'name'     => $category['name'],
+					'name'		 => $category['name'],
 					'children' => $children_data,
-					'column'   => $category['column'] ? $category['column'] : 1,
-					'href'     => $this->url->link('product/category', 'path=' . $category['category_id'])
+					'column'	 => $category['column'] ? $category['column'] : 1,
+					'href'		 => $this->url->link('product/category', 'path=' . $category['category_id'])
 				);
 			}
 		}
@@ -149,4 +150,5 @@ class ControllerCommonHeader extends Controller {
 
 		return $this->load->view('common/header', $data);
 	}
+
 }
