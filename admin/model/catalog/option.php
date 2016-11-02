24b88cdfd320a714ea979af38d91f94477a27873
buvetmaju.indeed.pro <buvetmaju.indeed.pro@new.indeed.pro>
2016-11-02 22:10:05 +0200

    Option autocomplete by filter_name

--- admin/model/catalog/option.php
+++ admin/model/catalog/option.php
@@ -71,7 +71,7 @@ class ModelCatalogOption extends Model {
 		$sql = "SELECT * FROM `" . DB_PREFIX . "option` o LEFT JOIN " . DB_PREFIX . "option_description od ON (o.option_id = od.option_id) WHERE od.language_id = '" . (int)$this->config->get('config_language_id') . "'";
 
 		if (!empty($data['filter_name'])) {
-			$sql .= " AND od.name LIKE '" . $this->db->escape($data['filter_name']) . "%'";
+			$sql .= " AND od.name LIKE '%" . $this->db->escape($data['filter_name']) . "%'";
 		}
 
 		$sort_data = array(
