0001dc9d8303a9626bf569edd67cda69d2f28072
copona.org <copona.org@new.indeed.pro>
2016-11-28 02:02:55 +0200

    Syntac in opencart.sql and gitignore .htaccess for install folder

--- install/opencart.sql
+++ install/opencart.sql
@@ -2347,21 +2347,21 @@ CREATE TABLE `oc_order_status` (
 -- Dumping data for table `oc_order_status`
 --
 
-INSERT INTO `oc_order_status` (`order_status_id`, `language_id`, `name`) VALUES
-(2, 1, 'Processing', '', ''),
-(3, 1, 'Shipped', '', ''),
-(7, 1, 'Canceled', '', ''),
-(5, 1, 'Complete', '', ''),
-(8, 1, 'Denied', '', ''),
-(9, 1, 'Canceled Reversal', '', ''),
-(10, 1, 'Failed', '', ''),
-(11, 1, 'Refunded', '', ''),
-(12, 1, 'Reversed', '', ''),
-(13, 1, 'Chargeback', '', ''),
-(1, 1, 'Pending', '', ''),
-(16, 1, 'Voided', '', ''),
-(15, 1, 'Processed', '', ''),
-(14, 1, 'Expired', '', '');
+INSERT INTO `oc_order_status` (`order_status_id`, `language_id`, `name`, `send_invoice`) VALUES
+(2, 1, 'Processing', ''),
+(3, 1, 'Shipped', ''),
+(7, 1, 'Canceled', ''),
+(5, 1, 'Complete', ''),
+(8, 1, 'Denied', ''),
+(9, 1, 'Canceled Reversal', ''),
+(10, 1, 'Failed', ''),
+(11, 1, 'Refunded', ''),
+(12, 1, 'Reversed', ''),
+(13, 1, 'Chargeback', ''),
+(1, 1, 'Pending', ''),
+(16, 1, 'Voided', ''),
+(15, 1, 'Processed', ''),
+(14, 1, 'Expired', '');
 
 -- --------------------------------------------------------
 
