9694aef4f9a79554d09d238af028f88d3a9612ba
Arnis Juraga <arnis.juraga@gmail.com>
2017-01-03 11:06:18 +0200

    Store extensions - yet raw material from Opencart

--- admin/language/en-gb/extension/store.php
+++ admin/language/en-gb/extension/store.php
@@ -4,10 +4,12 @@ $_['heading_title'] = 'Extension Store';
 
 // Text
 $_['text_success'] = 'Success: You have modified extensions!';
-$_['text_list'] = 'Extension List';
 $_['text_license'] = 'License';
+$_['text_price'] = 'Price';
 $_['text_free'] = 'Free';
 $_['text_commercial'] = 'Commercial';
+$_['text_rating'] = 'Rating';
+$_['text_compatibility'] = 'Compatibility';
 $_['text_category'] = 'All Categories';
 $_['text_theme'] = 'Themes';
 $_['text_payment'] = 'Payment Gateways';
@@ -18,5 +20,17 @@ $_['text_feed'] = 'Product Feeds';
 $_['text_report'] = 'Reports';
 $_['text_other'] = 'Other';
 
+// Tab
+$_['tab_description'] = 'Description';
+$_['tab_documentation'] = 'Documentation';
+$_['tab_download'] = 'Download';
+$_['tab_comment'] = 'Comment';
+
 // Error
 $_['error_permission'] = 'Warning: You do not have permission to modify extensions!';
+
+$_['text_list'] = 'Extension List';
+$_['text_all'] = 'All';
+$_['text_paid'] = 'Paid';
+$_['text_search'] = 'Search';
+
