d1fa7de15ded0f2d114c9377c9977132c47b1013
Copona DEV server1 <arnis.juraga@gmail.com>
2017-01-08 18:00:10 +0200

    Fixed correctly with reference function https://github.com/opencart/opencart/issues/5130

--- install/cli_install.php
+++ install/cli_install.php
@@ -28,7 +28,7 @@ error_reporting(E_ALL);
 $argv = $_SERVER['argv'];
 $script = array_shift($argv);
 $subcommand = array_shift($argv);
-$options = get_options($argv);
+$options = get_options($argv); 
 
 // DIR
 define('DIR_APPLICATION', str_replace('\\', '/', realpath(dirname(__FILE__))) . '/');
@@ -107,7 +107,7 @@ function get_options($argv) {
 }
 
 
-function valid($options) {
+function valid(&$options) {
 	$required = array(
 		'db_hostname',
 		'db_username',
