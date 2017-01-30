7e7b000bccf99c4969a679786858e0b913a66683
Arnis Juraga <arnis@indeed.pro>
2017-01-30 15:11:00 +0200

    Added category path by product_id and in catalog, when opening product/category controller without 'path', it shows all products

--- catalog/controller/product/category.php
+++ catalog/controller/product/category.php
@@ -362,6 +362,70 @@ class ControllerProductCategory extends Controller {
 			$this->response->setOutput($this->load->view('product/category', $data));
 		} elseif (!$category_info && !isset($this->request->get['path'])) {
 
+			$filter_data = array();
+			$product_total = $this->model_catalog_product->getTotalProducts($filter_data);
+			$results = $this->model_catalog_product->getProducts($filter_data);
+
+
+
+			foreach ($results as $result) {
+				$category_path = $this->model_catalog_category->getCategoryPath($category_id = 0, $result['product_id']);
+
+				if ($result['image']) {
+					$image = $this->model_tool_image->resize($result['image'], $this->config->get($this->config->get('config_theme') . '_image_product_width'), $this->config->get($this->config->get('config_theme') . '_image_product_height'));
+				} else {
+					$image = $this->model_tool_image->resize('placeholder.png', $this->config->get($this->config->get('config_theme') . '_image_product_width'), $this->config->get($this->config->get('config_theme') . '_image_product_height'));
+				}
+
+				if ($this->customer->isLogged() || !$this->config->get('config_customer_price')) {
+					$price = $this->currency->format($this->tax->calculate($result['price'], $result['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
+				} else {
+					$price = false;
+				}
+
+				if ((float)$result['special']) {
+					$special = $this->currency->format($this->tax->calculate($result['special'], $result['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
+				} else {
+					$special = false;
+				}
+
+				if ($this->config->get('config_tax')) {
+					$tax = $this->currency->format((float)$result['special'] ? $result['special'] : $result['price'], $this->session->data['currency']);
+				} else {
+					$tax = false;
+				}
+
+				if ($this->config->get('config_review_status')) {
+					$rating = (int)$result['rating'];
+				} else {
+					$rating = false;
+				}
+
+				$data['products'][] = array(
+					'product_id'	 => $result['product_id'],
+					'thumb'				 => $image,
+					'name'				 => $result['name'],
+					'description'	 => utf8_substr(strip_tags(html_entity_decode($result['description'], ENT_QUOTES, 'UTF-8')), 0, $this->config->get($this->config->get('config_theme') . '_product_description_length')) . '..',
+					'price'				 => $price,
+					'special'			 => $special,
+					'tax'					 => $tax,
+					'minimum'			 => $result['minimum'] > 0 ? $result['minimum'] : 1,
+					'rating'			 => $result['rating'],
+					'href'				 => $this->url->link('product/product', ($category_path ? 'path=' . $category_path : '' ) . '&product_id=' . $result['product_id'])
+				);
+			}
+			$this->document->setTitle($this->language->get('text_error'));
+			$this->response->addHeader($this->request->server['SERVER_PROTOCOL'] . ' 404 Not Found');
+			$data['continue'] = $this->url->link('common/home');
+
+			$data['column_left'] = $this->load->controller('common/column_left');
+			$data['column_right'] = $this->load->controller('common/column_right');
+			$data['content_top'] = $this->load->controller('common/content_top');
+			$data['content_bottom'] = $this->load->controller('common/content_bottom');
+			$data['footer'] = $this->load->controller('common/footer');
+			$data['header'] = $this->load->controller('common/header');
+
+			$this->response->setOutput($this->load->view('product/category', $data));
 		} else {
 			$url = '';
 
