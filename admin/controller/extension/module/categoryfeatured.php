cc40469b02d48c14f0cde3657897ea705385e3a2
Arnis <arnis.juraga@gmail.com>
2017-02-01 18:26:52 +0200

    Featured Category module from @rupaknepali

---
+++ admin/controller/extension/module/categoryfeatured.php
@@ -0,0 +1,229 @@
+<?php
+class ControllerExtensionModuleCategoryfeatured extends Controller {
+	private $error = array();
+
+	public function index() {
+		$this->load->language('extension/module/categoryfeatured');
+
+		$this->document->setTitle($this->language->get('heading_title'));
+
+		$this->load->model('extension/module');
+
+		if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validate()) {
+			if (!isset($this->request->get['module_id'])) {
+				$this->model_extension_module->addModule('categoryfeatured', $this->request->post);
+			} else {
+				$this->model_extension_module->editModule($this->request->get['module_id'], $this->request->post);
+			}
+
+			$this->session->data['success'] = $this->language->get('text_success');
+
+			$this->response->redirect($this->url->link('extension/extension', 'token=' . $this->session->data['token'] . '&type=module', true));
+		}
+
+		$data['heading_title'] = $this->language->get('heading_title');
+
+		$data['text_edit'] = $this->language->get('text_edit');
+		$data['text_enabled'] = $this->language->get('text_enabled');
+		$data['text_disabled'] = $this->language->get('text_disabled');
+
+		$data['entry_name'] = $this->language->get('entry_name');
+		$data['entry_category'] = $this->language->get('entry_category');
+		$data['entry_limit'] = $this->language->get('entry_limit');
+		$data['entry_width'] = $this->language->get('entry_width');
+		$data['entry_height'] = $this->language->get('entry_height');
+		$data['entry_status'] = $this->language->get('entry_status');
+
+		$data['help_category'] = $this->language->get('help_category');
+
+		$data['button_save'] = $this->language->get('button_save');
+		$data['button_cancel'] = $this->language->get('button_cancel');
+
+		if (isset($this->error['warning'])) {
+			$data['error_warning'] = $this->error['warning'];
+		} else {
+			$data['error_warning'] = '';
+		}
+
+		if (isset($this->error['name'])) {
+			$data['error_name'] = $this->error['name'];
+		} else {
+			$data['error_name'] = '';
+		}
+
+		if (isset($this->error['width'])) {
+			$data['error_width'] = $this->error['width'];
+		} else {
+			$data['error_width'] = '';
+		}
+
+		if (isset($this->error['height'])) {
+			$data['error_height'] = $this->error['height'];
+		} else {
+			$data['error_height'] = '';
+		}
+
+		$data['breadcrumbs'] = array();
+
+		$data['breadcrumbs'][] = array(
+			'text' => $this->language->get('text_home'),
+			'href' => $this->url->link('common/dashboard', 'token=' . $this->session->data['token'], true)
+		);
+
+		$data['breadcrumbs'][] = array(
+			'text' => $this->language->get('text_extension'),
+			'href' => $this->url->link('extension/extension', 'token=' . $this->session->data['token'] . '&type=module', true)
+		);
+
+		if (!isset($this->request->get['module_id'])) {
+			$data['breadcrumbs'][] = array(
+				'text' => $this->language->get('heading_title'),
+				'href' => $this->url->link('extension/module/categoryfeatured', 'token=' . $this->session->data['token'], true)
+			);
+		} else {
+			$data['breadcrumbs'][] = array(
+				'text' => $this->language->get('heading_title'),
+				'href' => $this->url->link('extension/module/categoryfeatured', 'token=' . $this->session->data['token'] . '&module_id=' . $this->request->get['module_id'], true)
+			);
+		}
+
+		if (!isset($this->request->get['module_id'])) {
+			$data['action'] = $this->url->link('extension/module/categoryfeatured', 'token=' . $this->session->data['token'], true);
+		} else {
+			$data['action'] = $this->url->link('extension/module/categoryfeatured', 'token=' . $this->session->data['token'] . '&module_id=' . $this->request->get['module_id'], true);
+		}
+
+		$data['cancel'] = $this->url->link('extension/extension', 'token=' . $this->session->data['token'] . '&type=module', true);
+
+		if (isset($this->request->get['module_id']) && ($this->request->server['REQUEST_METHOD'] != 'POST')) {
+			$module_info = $this->model_extension_module->getModule($this->request->get['module_id']);
+		}
+
+		$data['token'] = $this->session->data['token'];
+
+		if (isset($this->request->post['name'])) {
+			$data['name'] = $this->request->post['name'];
+		} elseif (!empty($module_info)) {
+			$data['name'] = $module_info['name'];
+		} else {
+			$data['name'] = '';
+		}
+
+		$this->load->model('catalog/category');
+
+		$data['categories'] = array();
+
+		if (!empty($this->request->post['category'])) {
+			$categories = $this->request->post['category'];
+		} elseif (!empty($module_info['category'])) {
+			$categories = $module_info['category'];
+		} else {
+			$categories = array();
+		}
+
+		foreach ($categories as $category_id) {
+			$category_info = $this->model_catalog_category->getCategory($category_id);
+
+			if ($category_info) {
+				$data['categories'][] = array(
+					'category_id' => $category_info['category_id'],
+					'name'       => $category_info['name']
+				);
+			}
+		}
+
+		if (isset($this->request->post['limit'])) {
+			$data['limit'] = $this->request->post['limit'];
+		} elseif (!empty($module_info)) {
+			$data['limit'] = $module_info['limit'];
+		} else {
+			$data['limit'] = 5;
+		}
+
+		if (isset($this->request->post['width'])) {
+			$data['width'] = $this->request->post['width'];
+		} elseif (!empty($module_info)) {
+			$data['width'] = $module_info['width'];
+		} else {
+			$data['width'] = 200;
+		}
+
+		if (isset($this->request->post['height'])) {
+			$data['height'] = $this->request->post['height'];
+		} elseif (!empty($module_info)) {
+			$data['height'] = $module_info['height'];
+		} else {
+			$data['height'] = 200;
+		}
+
+		if (isset($this->request->post['status'])) {
+			$data['status'] = $this->request->post['status'];
+		} elseif (!empty($module_info)) {
+			$data['status'] = $module_info['status'];
+		} else {
+			$data['status'] = '';
+		}
+
+		$data['header'] = $this->load->controller('common/header');
+		$data['column_left'] = $this->load->controller('common/column_left');
+		$data['footer'] = $this->load->controller('common/footer');
+
+		$this->response->setOutput($this->load->view('extension/module/categoryfeatured', $data));
+	}
+
+	protected function validate() {
+		if (!$this->user->hasPermission('modify', 'extension/module/categoryfeatured')) {
+			$this->error['warning'] = $this->language->get('error_permission');
+		}
+
+		if ((utf8_strlen($this->request->post['name']) < 3) || (utf8_strlen($this->request->post['name']) > 64)) {
+			$this->error['name'] = $this->language->get('error_name');
+		}
+
+		if (!$this->request->post['width']) {
+			$this->error['width'] = $this->language->get('error_width');
+		}
+
+		if (!$this->request->post['height']) {
+			$this->error['height'] = $this->language->get('error_height');
+		}
+
+		return !$this->error;
+	}
+
+    public function autocomplete() {
+        $json = array();
+
+        if (isset($this->request->get['filter_name'])) {
+            $this->load->model('catalog/category');
+
+            $filter_data = array(
+                'filter_name' => $this->request->get['filter_name'],
+                'sort'        => 'name',
+                'order'       => 'ASC',
+                'start'       => 0,
+                'limit'       => 5
+            );
+
+            $results = $this->model_catalog_category->getCategories($filter_data);
+
+            foreach ($results as $result) {
+                $json[] = array(
+                    'category_id' => $result['category_id'],
+                    'name'        => strip_tags(html_entity_decode($result['name'], ENT_QUOTES, 'UTF-8'))
+                );
+            }
+        }
+
+        $sort_order = array();
+
+        foreach ($json as $key => $value) {
+            $sort_order[$key] = $value['name'];
+        }
+
+        array_multisort($sort_order, SORT_ASC, $json);
+
+        $this->response->addHeader('Content-Type: application/json');
+        $this->response->setOutput(json_encode($json));
+    }
+}
---
+++ catalog/view/theme/default/template/extension/module/categoryfeatured.tpl
@@ -0,0 +1,17 @@
+<h3><?php echo $heading_title; ?></h3>
+<div class="row">
+  <?php foreach ($categories as $category) { ?>
+  <div class="category-layout col-lg-3 col-md-3 col-sm-6 col-xs-12">
+    <div class="category-thumb transition">
+      <div class="image">
+        <a href="<?php echo $category['href']; ?>">
+          <img src="<?php echo $category['thumb']; ?>" alt="<?php echo $category['name']; ?>" title="<?php echo $category['name']; ?>" class="img-responsive" />
+        </a>
+      </div>
+      <div class="caption">
+        <h4><a href="<?php echo $category['href']; ?>"><?php echo $category['name']; ?></a></h4>
+      </div>
+    </div>
+  </div>
+  <?php } ?>
+</div>
