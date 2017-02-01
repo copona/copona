9406ee68e8ee233612331ff44f58b31c94fa8879
Arnis <arnis.juraga@gmail.com>
2017-02-01 19:35:03 +0200

    Update fixes

--- update.php
+++ update.php
@@ -26,21 +26,39 @@ $db = new DB(DB_DRIVER, DB_HOSTNAME, DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_P
 $count = 0;
 
 echo "<pre>";
+
+// **************************
 // 2017-02-01
 // add TOP to information table
-if (!$db->query("SHOW COLUMNS FROM " . DB_PREFIX . "information LIKE 'top'")->num_rows) {
-	$sql = "ALTER TABLE `" . DB_PREFIX . "information` ADD `top` INT(1) NOT NULL DEFAULT '0' AFTER `bottom`";
+
+$table = DB_PREFIX . "information";
+$column = 'top';
+if (!$db->query("SHOW COLUMNS FROM `" . $table . "` LIKE '" . $column . "'")->num_rows) {
+	$sql = "ALTER TABLE `" . $table . "` ADD `" . $column . "` INT(1) NOT NULL DEFAULT '0' AFTER `bottom`";
 	echo $sql . "<br /> // started ...";
 	$db->query($sql);
 	echo "finished\n</br>";
 	$count++;
 }
 
+// **************************
 // 2017-02-01
 // Images for Information pages
+// Create table if not exists.
+$table = DB_PREFIX . "information_image";
+if (!$db->query("SHOW TABLES LIKE '" . $table . "'")->num_rows) {
+	$sql = "CREATE TABLE IF NOT EXISTS `" . $table . "` ( `information_image_id` INT NOT NULL AUTO_INCREMENT , `information_id` INT NOT NULL , `image`
+	VARCHAR(255) NOT NULL , `sort_order` INT(3) NOT NULL , PRIMARY KEY (`information_image_id`)) COLLATE utf8_general_ci";
+	echo $sql . "<br /> // started ...";
+	$db->query($sql);
+	echo "finished\n</br>";
+	$count++;
+}
 // ALTER TABLE `oc_information` ADD `image` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER `information_id`;
-if (!$db->query("SHOW COLUMNS FROM " . DB_PREFIX . "information LIKE 'image'")->num_rows) {
-	$sql = "ALTER TABLE `" . DB_PREFIX . "information` ADD `image` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER `information_id`";
+$table = DB_PREFIX . "information";
+$column = 'image';
+if (!$db->query("SHOW COLUMNS FROM `" . $table . "` LIKE '" . $column . "'")->num_rows) {
+	$sql = "ALTER TABLE `" . $table . "` ADD `" . $column . "` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER `information_id`";
 	echo $sql . "<br /> // started ...";
 	$db->query($sql);
 	echo "finished\n</br>";
