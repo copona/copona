82ebde372a46bc9c49426184a1fae8d8f3de302f
lucasjkr <lucas@lucaskrupinski.com>
2017-02-17 13:06:59 -0500

    checking for PHP 5.6.0 rather than 5.4

--- install/controller/install/step_2.php
+++ install/controller/install/step_2.php
@@ -72,7 +72,7 @@ class ControllerInstallStep2 extends Controller {
 	}
 
 	private function validate() {
-		if (phpversion() < '5.4') {
+		if (phpversion() < '5.6') {
 			$this->error['warning'] = $this->language->get('error_version');
 		}
 
