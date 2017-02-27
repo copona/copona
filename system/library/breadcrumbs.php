0454e0de6c2f600b26ee51319089e2ecc0967702
Arnis Juraga <arnis.juraga@gmail.com>
2017-02-27 13:05:03 +0200

    Breadcrumbs reformat

--- system/library/breadcrumbs.php
+++ system/library/breadcrumbs.php
@@ -2,55 +2,50 @@
 // author wazzzar
 
 class Breadcrumbs {
+    private $path = array();
+    private $registry;
 
-	private $path = array();
-	private $registry;
-	
-	public function __construct( $registry ){
-	
-		$this->registry = $registry;
-		$this->push( 'text_home', 'common/home' );
-	
-	}
-	
-	public function push( $text, $route ){
-	
-		$this->path[] = array(
-			'text' => $this->registry->language->get( (string) $text ),
-			'href' => $this->registry->url->link( (string) $route )
-		);
-	
-	}
-	
-	public function render(){
-		// if in path only home link
-		if( count( $this->path ) == 1 ) return null;
-	
-		$html = '<ul class="breadcrumb">';
-		foreach ( $this->path as $part ) {
-			$html .= '<li><a href="' . $part['href'] .'">'. $part['text'] .'</a></li>';
-		}
-		$html .= '</ul>';
-		return $html;
-	
-	}
-	
-	public function streem(){
-		
-		echo $this->render();
-		
-	}
-	
-	public function getPath(){
-		// if in path only home link
-		if( count( $this->path ) == 1 ) return null;
-		
-		return $this->path;
-		
-	}
+    public function __construct($registry) {
+
+        $this->registry = $registry;
+        $this->push('text_home', 'common/home');
+    }
+
+    public function push($text, $route) {
+
+        $this->path[] = array(
+            'text' => $this->registry->language->get((string)$text),
+            'href' => $this->registry->url->link((string)$route)
+        );
+    }
+
+    public function render() {
+        // if in path only home link
+        if (count($this->path) == 1)
+            return null;
+
+        $html = '<ul class="breadcrumb">';
+        foreach ($this->path as $part) {
+            $html .= '<li><a href="' . $part['href'] . '">' . $part['text'] . '</a></li>';
+        }
+        $html .= '</ul>';
+        return $html;
+    }
+
+    public function streem() {
+
+        echo $this->render();
+    }
+
+    public function getPath() {
+        // if in path only home link
+        if (count($this->path) == 1)
+            return null;
+
+        return $this->path;
+    }
 
 }
-
 /* example
 // using new object
 $bread_crumbs = new Breadcrumbs( $this );
