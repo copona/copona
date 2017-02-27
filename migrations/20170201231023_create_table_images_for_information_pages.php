62e12afc5f9ba25817b4f52190a7df82371ce3aa
Arnis Juraga <arnis.juraga@gmail.com>
2017-02-27 13:03:05 +0200

    Introducing Content Meta table. Lets try this

--- migrations/20170201231023_create_table_images_for_information_pages.php
+++ migrations/20170201231023_create_table_images_for_information_pages.php
@@ -2,22 +2,22 @@
 
 use Phinx\Migration\AbstractMigration;
 
-class CreateTableImagesForInformationPages extends AbstractMigration
-{
+class CreateTableImagesForInformationPages extends AbstractMigration {
+
     /**
      * Images for Information pages
      * Create table if not exists.
      */
-    public function change()
-    {
+    public function change() {
         $exists = $this->hasTable('information_image');
         if (!$exists) {
-            $information_image = $this->table('information_image', ['id' => 'information_image_id']);
+            $information_image = $this->table('information_image', ['id' => 'information_image_id' ]);
             $information_image
-                ->addColumn('information_id', 'integer', ['null' => false, 'limit' => 11])
-                ->addColumn('image', 'string', ['null' => false, 'limit' => 255])
-                ->addColumn('sort_order', 'integer', ['null' => false, 'limit' => 3])
+                ->addColumn('information_id', 'integer', ['null' => false, 'limit' => 11 ])
+                ->addColumn('image', 'string', ['null' => false, 'limit' => 255 ])
+                ->addColumn('sort_order', 'integer', ['null' => false, 'limit' => 3 ])
                 ->save();
         }
     }
+
 }
