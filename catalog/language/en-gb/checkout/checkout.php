a400691e7f31a5b3bea2fe5ec2af4dea34b024e2
Arnis Juraga <arnis.juraga@gmail.com>
2017-03-07 20:07:37 +0200

    checkout/checkout translations

--- catalog/language/en-gb/checkout/checkout.php
+++ catalog/language/en-gb/checkout/checkout.php
@@ -34,6 +34,7 @@ $_['text_payment_recurring'] = 'Payment Profile';
 $_['text_trial_description'] = '%s every %d %s(s) for %d payment(s) then';
 $_['text_payment_description'] = '%s every %d %s(s) for %d payment(s)';
 $_['text_payment_cancel'] = '%s every %d %s(s) until canceled';
+$_['payer_data_title'] = 'Payment details';
 $_['text_day'] = 'day';
 $_['text_week'] = 'week';
 $_['text_semi_month'] = 'half-month';
