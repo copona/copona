1f8ab9136118da0b7b1872db91a09b1aa007fca8
Arnis <arnis.juraga@gmail.com>
2017-03-14 21:10:32 +0200

    Language load in admin shipping controllers

--- admin/controller/extension/shipping/auspost.php
+++ admin/controller/extension/shipping/auspost.php
@@ -3,7 +3,7 @@ class ControllerExtensionShippingAusPost extends Controller {
     private $error = array();
 
     public function index() {
-        $this->load->language('extension/shipping/auspost');
+        $data = $this->load->language('extension/shipping/auspost');
 
         $this->document->setTitle($this->language->get('heading_title'));
 
@@ -18,28 +18,6 @@ class ControllerExtensionShippingAusPost extends Controller {
         }
 
         $data['heading_title'] = $this->language->get('heading_title');
-
-        $data['text_edit'] = $this->language->get('text_edit');
-        $data['text_enabled'] = $this->language->get('text_enabled');
-        $data['text_disabled'] = $this->language->get('text_disabled');
-        $data['text_all_zones'] = $this->language->get('text_all_zones');
-        $data['text_none'] = $this->language->get('text_none');
-
-        $data['entry_postcode'] = $this->language->get('entry_postcode');
-        $data['entry_standard'] = $this->language->get('entry_standard');
-        $data['entry_express'] = $this->language->get('entry_express');
-        $data['entry_display_time'] = $this->language->get('entry_display_time');
-        $data['entry_weight_class'] = $this->language->get('entry_weight_class');
-        $data['entry_tax_class'] = $this->language->get('entry_tax_class');
-        $data['entry_geo_zone'] = $this->language->get('entry_geo_zone');
-        $data['entry_status'] = $this->language->get('entry_status');
-        $data['entry_sort_order'] = $this->language->get('entry_sort_order');
-
-        $data['help_display_time'] = $this->language->get('help_display_time');
-        $data['help_weight_class'] = $this->language->get('help_weight_class');
-
-        $data['button_save'] = $this->language->get('button_save');
-        $data['button_cancel'] = $this->language->get('button_cancel');
 
         if (isset($this->error['warning'])) {
             $data['error_warning'] = $this->error['warning'];
