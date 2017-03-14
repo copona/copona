42ec9426c28db6ad5f687de56430890cb440dd5e
Janis Travins <janis.travins@indeed.pro>
2017-03-14 15:02:56 +0200

    additional fields for latest

--- catalog/controller/extension/module/latest.php
+++ catalog/controller/extension/module/latest.php
@@ -19,8 +19,8 @@ class ControllerExtensionModuleLatest extends Controller {
         $data['products'] = array();
 
         $filter_data = array(
-            'sort'  => 'p.date_added',
-            'order' => 'DESC',
+            'sort'  => 'p.sort_order',
+            'order' => 'ASC',
             'start' => 0,
             'limit' => $setting['limit']
         );
@@ -59,16 +59,58 @@ class ControllerExtensionModuleLatest extends Controller {
                     $rating = false;
                 }
 
+
+                $group_products_data = array();
+                if ($result['product_group_id']) {
+
+                    $filter = array(
+                        'group_products'   => true,
+                        'product_group_id' => $result['product_group_id'],
+                        'product_id'       => $result['product_id']
+                    );
+                    $group_products = $this->model_catalog_product->getProducts($filter);
+                    foreach ($group_products as $group_product) {
+                        if ($group_product['product_id'] == $result['product_id'])
+                            continue;
+
+                        if ($this->customer->isLogged() || !$this->config->get('config_customer_price')) {
+                            $group_price = $this->currency->format($this->tax->calculate($group_product['price'], $result['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
+                        } else {
+                            $group_price = false;
+                        }
+
+                        if ($group_product['image']) {
+                            $group_image = $this->model_tool_image->cropsize($group_product['image'], $this->config->get($this->config->get('config_theme') . '_image_product_group_width'), $this->config->get($this->config->get('config_theme') . '_image_product_group_height'));
+                        } else {
+                            $group_image = $this->model_tool_image->cropsize('placeholder.png', $this->config->get($this->config->get('config_theme') . '_image_product_group_width'), $this->config->get($this->config->get('config_theme') . '_image_product_group_height'));
+                        }
+
+                        $group_products_data[] = array(
+                            'product_id'       => $group_product['product_id'],
+                            'product_group_id' => $group_product['product_group_id'],
+                            'name'             => $group_product['name'],
+                            'description'      => html_entity_decode($group_product['description'], ENT_QUOTES, 'UTF-8'),
+                            'product_price'    => $group_product['price'],
+                            'image'            => $group_image,
+                            'price'            => $group_price,
+                            'href'             => $this->url->link('product/product', 'product_id=' . $group_product['product_id'])
+                        );
+                    }
+                }
                 $data['products'][] = array(
-                    'product_id'  => $result['product_id'],
-                    'thumb'       => $image,
-                    'name'        => $result['name'],
-                    'description' => utf8_substr(strip_tags(html_entity_decode($result['description'], ENT_QUOTES, 'UTF-8')), 0, $this->config->get($this->config->get('config_theme') . '_product_description_length')) . '..',
-                    'price'       => $price,
-                    'special'     => $special,
-                    'tax'         => $tax,
-                    'rating'      => $rating,
-                    'href'        => $this->url->link('product/product', 'product_id=' . $result['product_id'])
+                    'product_id'           => $result['product_id'],
+                    'thumb'                => $image,
+                    'name'                 => $result['name'],
+                    'stripped_description' => utf8_substr(strip_tags(html_entity_decode($result['description'], ENT_QUOTES, 'UTF-8')), 0, $this->config->get($this->config->get('config_theme') . '_product_description_length')) . '..',
+                    'description'          => html_entity_decode($result['description'], ENT_QUOTES, 'UTF-8'),
+                    'price'                => $price,
+                    'product_price'        => $result['price'],
+                    'special'              => $special,
+                    'tax'                  => $tax,
+                    'sku'                  => $result['sku'],
+                    'rating'               => $rating,
+                    'href'                 => $this->url->link('product/product', 'product_id=' . $result['product_id']),
+                    'group_products'       => $group_products_data
                 );
             }
 
