ec091dc18b173e1544e09c5488395dc56d5f0c59
Arnis Juraga <arnis.juraga@gmail.com>
2017-03-20 17:44:11 +0200

    Reformat, naming

--- catalog/view/theme/simplica/functions.php
+++ catalog/view/theme/simplica/functions.php
@@ -2,12 +2,12 @@
 /* * * Theme specific - override Settings !  * * */
 !defined('DIR_SYSTEM') ? die() : false;
 // Example:
-$rozesbode_settings = array(
+$template_config_settings = array(
     // 'theme_default_image_category_width'       => 80,
     // 'theme_default_image_category_height'      => 80,
 );
 
-foreach ($rozesbode_settings as $key => $val) {
+foreach ($template_config_settings as $key => $val) {
     $this->config->set($key, $val);
 }
 
