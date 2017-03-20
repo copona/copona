0b43ac076a4857134368a1ab0abfbbaeb52824e7
Arnis <arnis.juraga@gmail.com>
2017-03-20 12:39:44 +0200

    fixed product image thumb resize

--- catalog/controller/product/product.php
+++ catalog/controller/product/product.php
@@ -273,7 +273,7 @@ class ControllerProductProduct extends Controller {
             }
 
             if ($product_info['image']) {
-                $data['thumb'] = $this->model_tool_image->propsize($product_info['image'], $this->config->get($this->config->get('config_theme') . '_image_additional_width'), $this->config->get($this->config->get('config_theme') . '_image_additional_height'));
+                $data['thumb'] = $this->model_tool_image->propsize($product_info['image'], $this->config->get($this->config->get('config_theme') . '_image_thumb_width'), $this->config->get($this->config->get('config_theme') . '_image_thumb_height'));
             } else {
                 $data['thumb'] = '';
             }
