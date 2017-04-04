6434b9e60adef295da02b67cbfbeafdba57159f3
Janis Travins <janis.travins@indeed.pro>
2017-04-04 22:04:08 +0300

    Changes in default config values. Added config for Product Category list resize method

--- catalog/view/theme/default/functions.php
+++ catalog/view/theme/default/functions.php
@@ -1,12 +1,18 @@
 <?php
+/*
+ * This file will be included at catalog/controller/startup.php as first priority
+ * Right before inclusion of Theme specific functions.php
+ *
+ */
 /* * * Theme specific - override Settings !  * * */
 !defined('DIR_SYSTEM') ? die() : false;
 // Example:
 $template_config_settings = array(
+    'theme_default_product_category_list_resize' => 'resize',
     // 'theme_default_image_category_width'       => 80,
     // 'theme_default_image_category_height'      => 80,
 );
 
 foreach ($template_config_settings as $key => $val) {
     $this->config->set($key, $val);
-} 
+}
