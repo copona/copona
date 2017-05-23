180bed55c6480f1b63a547c367ce37febfc8c02e
Jekabs <jekabs.kalnins@indeed.pro>
2017-05-23 18:08:37 +0300

    Admin header language simplify

--- admin/controller/common/header.php
+++ admin/controller/common/header.php
@@ -2,6 +2,9 @@
 class ControllerCommonHeader extends Controller {
 
     public function index() {
+
+        $data = $this->load->language('common/header');
+
         $data['title'] = $this->document->getTitle();
 
         if ($this->request->server['HTTPS']) {
@@ -19,29 +22,9 @@ class ControllerCommonHeader extends Controller {
         $data['lang'] = $this->language->get('code');
         $data['direction'] = $this->language->get('direction');
 
-        $this->load->language('common/header');
-
         $data['heading_title'] = $this->language->get('heading_title');
 
-        $data['text_order'] = $this->language->get('text_order');
-        $data['text_processing_status'] = $this->language->get('text_processing_status');
-        $data['text_complete_status'] = $this->language->get('text_complete_status');
-        $data['text_return'] = $this->language->get('text_return');
-        $data['text_customer'] = $this->language->get('text_customer');
-        $data['text_online'] = $this->language->get('text_online');
-        $data['text_approval'] = $this->language->get('text_approval');
-        $data['text_product'] = $this->language->get('text_product');
-        $data['text_stock'] = $this->language->get('text_stock');
-        $data['text_review'] = $this->language->get('text_review');
-        $data['text_affiliate'] = $this->language->get('text_affiliate');
-        $data['text_store'] = $this->language->get('text_store');
-        $data['text_front'] = $this->language->get('text_front');
-        $data['text_help'] = $this->language->get('text_help');
-        $data['text_homepage'] = $this->language->get('text_homepage');
-        $data['text_documentation'] = $this->language->get('text_documentation');
-        $data['text_support'] = $this->language->get('text_support');
         $data['text_logged'] = sprintf($this->language->get('text_logged'), $this->user->getUserName());
-        $data['text_logout'] = $this->language->get('text_logout');
 
         // Check if accessed over HTTPS connection:
 
