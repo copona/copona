efb2b69ad60df380154f07dd87373abc03e5529c
Arnis Juraga <arnis.juraga@gmail.com>
2017-06-08 17:20:47 +0300

    ps reformat

--- catalog/model/catalog/manufacturer.php
+++ catalog/model/catalog/manufacturer.php
@@ -1,13 +1,16 @@
 <?php
-class ModelCatalogManufacturer extends Model {
+class ModelCatalogManufacturer extends Model
+{
 
-    public function getManufacturer($manufacturer_id) {
+    public function getManufacturer($manufacturer_id)
+    {
         $query = $this->db->query("SELECT * FROM " . DB_PREFIX . "manufacturer m LEFT JOIN " . DB_PREFIX . "manufacturer_to_store m2s ON (m.manufacturer_id = m2s.manufacturer_id) WHERE m.manufacturer_id = '" . (int)$manufacturer_id . "' AND m2s.store_id = '" . (int)$this->config->get('config_store_id') . "'");
 
         return $query->row;
     }
 
-    public function getManufacturers($data = array()) {
+    public function getManufacturers($data = array())
+    {
         if ($data) {
             $sql = "SELECT * FROM " . DB_PREFIX . "manufacturer m LEFT JOIN " . DB_PREFIX . "manufacturer_to_store m2s ON (m.manufacturer_id = m2s.manufacturer_id) WHERE m2s.store_id = '" . (int)$this->config->get('config_store_id') . "'";
 
