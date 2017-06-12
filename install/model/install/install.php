1fc09e837ea4b98b7e596c939f53ee4ba3712682
kallef <prhost@prhost.com.br>
2017-06-12 16:34:04 -0300

    Executing migrations in instalation

--- install/model/install/install.php
+++ install/model/install/install.php
@@ -4,7 +4,9 @@ class ModelInstallInstall extends Model
 {
     public function database($data)
     {
-        $db = new DB($data['db_driver'], htmlspecialchars_decode($data['db_hostname']), htmlspecialchars_decode($data['db_username']), htmlspecialchars_decode($data['db_password']), htmlspecialchars_decode($data['db_database']), $data['db_port']);
+        $db = new DB($data['db_driver'], htmlspecialchars_decode($data['db_hostname']),
+          htmlspecialchars_decode($data['db_username']), htmlspecialchars_decode($data['db_password']),
+          htmlspecialchars_decode($data['db_database']), $data['db_port']);
 
         $file = DIR_APPLICATION . 'opencart.sql';
 
@@ -22,7 +24,8 @@ class ModelInstallInstall extends Model
                     $sql .= $line;
 
                     if (preg_match('/;\s*$/', $line)) {
-                        $sql = str_replace("DROP TABLE IF EXISTS `oc_", "DROP TABLE IF EXISTS `" . $data['db_prefix'], $sql);
+                        $sql = str_replace("DROP TABLE IF EXISTS `oc_", "DROP TABLE IF EXISTS `" . $data['db_prefix'],
+                          $sql);
                         $sql = str_replace("CREATE TABLE `oc_", "CREATE TABLE `" . $data['db_prefix'], $sql);
                         $sql = str_replace("INSERT INTO `oc_", "INSERT INTO `" . $data['db_prefix'], $sql);
                         $sql = str_replace("ALTER TABLE `oc_", "ALTER TABLE `" . $data['db_prefix'], $sql);
@@ -40,7 +43,8 @@ class ModelInstallInstall extends Model
 
             $db->query("DELETE FROM `" . $data['db_prefix'] . "user` WHERE user_id = '1'");
 
-            $db->query("INSERT INTO `" . $data['db_prefix'] . "user` SET user_id = '1', user_group_id = '1', username = '" . $db->escape($data['username']) . "', password = '" . password_hash($data['password'], PASSWORD_DEFAULT) . "', firstname = 'John', lastname = 'Doe', email = '" . $db->escape($data['email']) . "', status = '1', date_added = NOW()");
+            $db->query("INSERT INTO `" . $data['db_prefix'] . "user` SET user_id = '1', user_group_id = '1', username = '" . $db->escape($data['username']) . "', password = '" . password_hash($data['password'],
+                PASSWORD_DEFAULT) . "', firstname = 'John', lastname = 'Doe', email = '" . $db->escape($data['email']) . "', status = '1', date_added = NOW()");
             $db->query("DELETE FROM `" . $data['db_prefix'] . "setting` WHERE `key` = 'config_email'");
             $db->query("INSERT INTO `" . $data['db_prefix'] . "setting` SET `code` = 'config', `key` = 'config_email', value = '" . $db->escape($data['email']) . "'");
 
@@ -61,5 +65,35 @@ class ModelInstallInstall extends Model
                 $db->query("UPDATE `" . $data['db_prefix'] . "setting` SET value = '1' WHERE `key` = 'config_seo_url'");
             }
         }
+    }
+
+    /**
+     * Execute phinx migration
+     * @return string
+     */
+    public function migration($data)
+    {
+        define('DB_DRIVER', $data['db_driver']);
+        define('DB_HOSTNAME', htmlspecialchars_decode($data['db_hostname']));
+        define('DB_USERNAME', htmlspecialchars_decode($data['db_username']));
+        define('DB_PASSWORD', htmlspecialchars_decode($data['db_password']));
+        define('DB_DATABASE', htmlspecialchars_decode($data['db_database']));
+        define('DB_PORT', $data['db_port']);
+        define('DB_PREFIX', addslashes($this->request->post['db_prefix']));
+
+        //migrations path inside extension
+        $_SERVER['PHINX_MIGRATION_PATH'] = realpath(DIR_OPENCART . 'migrations');
+
+        //init phinx
+        $app = new \Phinx\Console\PhinxApplication();
+        $wrap = new \Phinx\Wrapper\TextWrapper($app);
+
+        //set parser
+        $wrap->setOption('parser', 'php');
+
+        //set config file
+        $wrap->setOption('configuration', DIR_OPENCART . 'phinx.php');
+
+        return $wrap->getMigrate('default');
     }
 }
--- phinx.php
+++ phinx.php
@@ -1,24 +1,26 @@
 <?php
 
-require_once __DIR__ . "/config.php";
+if (!defined('DIR_OPENCART')) {
+    require_once __DIR__ . "/config.php";
+}
 
 $migration_path = __DIR__ . "/migrations";
 
 return [
-    "paths"        => [
-        "migrations" => $migration_path
-    ],
-    "environments" => [
-        "default_migration_table" => DB_PREFIX . "migrations",
-        "default_database"        => 'default',
-        'default'                 => [
-            "adapter"      => "mysql",
-            "host"         => DB_HOSTNAME,
-            "name"         => DB_DATABASE,
-            "user"         => DB_USERNAME,
-            "pass"         => DB_PASSWORD,
-            "port"         => DB_PORT,
-            "table_prefix" => DB_PREFIX
-        ]
+  "paths"        => [
+    "migrations" => $migration_path
+  ],
+  "environments" => [
+    "default_migration_table" => DB_PREFIX . "migrations",
+    "default_database"        => 'default',
+    'default'                 => [
+      "adapter"      => "mysql",
+      "host"         => DB_HOSTNAME,
+      "name"         => DB_DATABASE,
+      "user"         => DB_USERNAME,
+      "pass"         => DB_PASSWORD,
+      "port"         => DB_PORT,
+      "table_prefix" => DB_PREFIX
     ]
+  ]
 ];
