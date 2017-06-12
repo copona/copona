51dccb25c212cb60bfff7cf87ceeafc0c4289473
Arnis Juraga <arnis.juraga@gmail.com>
2017-06-12 16:26:00 +0300

    added banner description and added to slideshow module

--- admin/model/design/banner.php
+++ admin/model/design/banner.php
@@ -11,7 +11,7 @@ class ModelDesignBanner extends Model
         if (isset($data['banner_image'])) {
             foreach ($data['banner_image'] as $language_id => $value) {
                 foreach ($value as $banner_image) {
-                    $this->db->query("INSERT INTO " . DB_PREFIX . "banner_image SET banner_id = '" . (int)$banner_id . "', language_id = '" . (int)$language_id . "', title = '" . $this->db->escape($banner_image['title']) . "', link = '" . $this->db->escape($banner_image['link']) . "', image = '" . $this->db->escape($banner_image['image']) . "', sort_order = '" . (int)$banner_image['sort_order'] . "'");
+                    $this->db->query("INSERT INTO " . DB_PREFIX . "banner_image SET banner_id = '" . (int)$banner_id . "', language_id = '" . (int)$language_id . "', title = '" . $this->db->escape($banner_image['title']) . "', description = '" . $this->db->escape($banner_image['description']) . "', link = '" . $this->db->escape($banner_image['link']) . "', image = '" . $this->db->escape($banner_image['image']) . "', sort_order = '" . (int)$banner_image['sort_order'] . "'");
                 }
             }
         }
@@ -28,7 +28,7 @@ class ModelDesignBanner extends Model
         if (isset($data['banner_image'])) {
             foreach ($data['banner_image'] as $language_id => $value) {
                 foreach ($value as $banner_image) {
-                    $this->db->query("INSERT INTO " . DB_PREFIX . "banner_image SET banner_id = '" . (int)$banner_id . "', language_id = '" . (int)$language_id . "', title = '" . $this->db->escape($banner_image['title']) . "', link = '" . $this->db->escape($banner_image['link']) . "', image = '" . $this->db->escape($banner_image['image']) . "', sort_order = '" . (int)$banner_image['sort_order'] . "'");
+                    $this->db->query("INSERT INTO " . DB_PREFIX . "banner_image SET banner_id = '" . (int)$banner_id . "', language_id = '" . (int)$language_id . "', title = '" . $this->db->escape($banner_image['title']) . "', description = '" . $this->db->escape($banner_image['description']) . "', link = '" . $this->db->escape($banner_image['link']) . "', image = '" . $this->db->escape($banner_image['image']) . "', sort_order = '" . (int)$banner_image['sort_order'] . "'");
                 }
             }
         }
@@ -93,10 +93,11 @@ class ModelDesignBanner extends Model
 
         foreach ($banner_image_query->rows as $banner_image) {
             $banner_image_data[$banner_image['language_id']][] = array(
-                'title'      => $banner_image['title'],
-                'link'       => $banner_image['link'],
-                'image'      => $banner_image['image'],
-                'sort_order' => $banner_image['sort_order']
+                'title'       => $banner_image['title'],
+                'link'        => $banner_image['link'],
+                'image'       => $banner_image['image'],
+                'description' => $banner_image['description'],
+                'sort_order'  => $banner_image['sort_order']
             );
         }
 
--- catalog/view/theme/simplica/template/extension/module/slideshow.tpl
+++ catalog/view/theme/simplica/template/extension/module/slideshow.tpl
@@ -1,15 +1,23 @@
-<div id="slideshow<?php echo $module; ?>" class="owl-carousel" style="opacity: 1;">
+<div id="slideshow<?php echo $module; ?>" class="banner-slideshow owl-carousel" style="opacity: 1;">
     <?php foreach ($banners as $banner) { ?>
       <div class="item">
           <?php if ($banner['link']) { ?>
-        <a href="<?php echo $banner['link']; ?>"><img src="<?php echo $banner['image']; ?>" alt="<?php echo $banner['title']; ?>" class="img-responsive" /></a>
-        <?php } else { ?>
-        <img src="<?php echo $banner['image']; ?>" alt="<?php echo $banner['title']; ?>" class="img-responsive" />
-        <?php } ?>
+              <?php if ($banner['title']) { ?>
+                <img src="<?php echo $banner['image']; ?>" alt="<?php echo $banner['title']; ?>" class="img-responsive"/>
+                <a class="banner-text" href="<?=$banner['link'] ?>">
+                  <h2><?php echo $banner['title']; ?></h2>
+                  <?=($banner['description'] ? "<span>" . $banner['description'] . "</span>" : '' ); ?>
+                </a>
+              <?php } else { ?>
+                  <a href="<?php echo $banner['link']; ?>"><img src="<?php echo $banner['image']; ?>" alt="<?php echo $banner['title']; ?>" class="img-responsive"/></a>
+              <?php } ?>
+          <?php } else { ?>
+            <img src="<?php echo $banner['image']; ?>" alt="<?php echo $banner['title']; ?>" class="img-responsive"/>
+          <?php } ?>
       </div>
-      <?php } ?>
-    </div>
-    <script type="text/javascript"><!--
+    <?php } ?>
+</div>
+<script type="text/javascript"><!--
     $('#slideshow<?php echo $module; ?>').owlCarousel({
         items: 6,
         autoPlay: 3000,
@@ -18,4 +26,4 @@
         navigationText: ['<i class="fa fa-chevron-left fa-5x"></i>', '<i class="fa fa-chevron-right fa-5x"></i>'],
         pagination: true
     });
---></script>
+    --></script>
