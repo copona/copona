d76b957c9b87bc96add58be8da50744cef32dd2b
kallef <prhost@prhost.com.br>
2017-07-05 13:24:47 -0300

    The following should be somthing like 'image_base_url' => rtrim(BASE_URL_CATALOG, '/') . '/' . PATH_CACHE_PUBLIC . 'image/', in case install in subfolder. thanks @ADDCreative

--- config/general.php
+++ config/general.php
@@ -31,7 +31,7 @@ return [
      * You can modify the base url of the images
      * Do not add protocol https or http
      */
-    'image_base_url'     => DOMAIN_NAME . '/' . PATH_CACHE_PUBLIC . 'image/',
+    'image_base_url'     => rtrim(BASE_URL_CATALOG, '/') . '/' . PATH_CACHE_PUBLIC . 'image',
 
     /**
      * Url
