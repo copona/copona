6582740d918a4c54dbf28332fdaf9c59a8ec4e91
Arnisj <arnis.juraga@gmail.com>
2017-08-29 21:46:18 +0300

    Simplified users creation in admin

--- admin/controller/user/user.php
+++ admin/controller/user/user.php
@@ -458,7 +458,7 @@ class ControllerUserUser extends Controller {
             $this->error['warning'] = $this->language->get('error_permission');
         }
 
-        if ((utf8_strlen($this->request->post['username']) < 3) || (utf8_strlen($this->request->post['username']) > 20)) {
+        if ((utf8_strlen($this->request->post['username']) < 3) || (utf8_strlen($this->request->post['username']) > 64)) {
             $this->error['username'] = $this->language->get('error_username');
         }
 
@@ -476,10 +476,6 @@ class ControllerUserUser extends Controller {
 
         if ((utf8_strlen(trim($this->request->post['firstname'])) < 1) || (utf8_strlen(trim($this->request->post['firstname'])) > 32)) {
             $this->error['firstname'] = $this->language->get('error_firstname');
-        }
-
-        if ((utf8_strlen(trim($this->request->post['lastname'])) < 1) || (utf8_strlen(trim($this->request->post['lastname'])) > 32)) {
-            $this->error['lastname'] = $this->language->get('error_lastname');
         }
 
         if ((utf8_strlen($this->request->post['email']) > 96) || !filter_var($this->request->post['email'], FILTER_VALIDATE_EMAIL)) {
