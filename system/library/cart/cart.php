8f0500193c5e2173ba4e5c0e1e37cb4a50637b2b
Jekabs Janis Kalnins <jekabs.kalnins@indeed.pro>
2017-09-11 14:19:33 +0300

    Some fixes on Tax calculation. Needs testing https://github.com/opencart/opencart/issues/5657

--- system/library/cart/cart.php
+++ system/library/cart/cart.php
@@ -358,13 +358,13 @@ class Cart {
 
         foreach ($this->cartProducts as $product) {
             if ($product['tax_class_id']) {
-                $tax_rates = $this->tax->getRates($product['price'], $product['tax_class_id']);
+                $tax_rates = $this->tax->getRates($product['total'], $product['tax_class_id']);
 
                 foreach ($tax_rates as $tax_rate) {
                     if (!isset($tax_data[$tax_rate['tax_rate_id']])) {
-                        $tax_data[$tax_rate['tax_rate_id']] = ($tax_rate['amount'] * $product['quantity']);
+                        $tax_data[$tax_rate['tax_rate_id']] = ($tax_rate['amount']);
                     } else {
-                        $tax_data[$tax_rate['tax_rate_id']] += ($tax_rate['amount'] * $product['quantity']);
+                        $tax_data[$tax_rate['tax_rate_id']] += ($tax_rate['amount']);
                     }
                 }
             }
@@ -377,7 +377,7 @@ class Cart {
         $total = 0;
 
         foreach ($this->cartProducts as $product) {
-            $total += $this->tax->calculate($product['price'], $product['tax_class_id'], $this->config->get('config_tax')) * $product['quantity'];
+            $total += $this->tax->calculate($product['total'], $product['tax_class_id'], $this->config->get('config_tax'));
         }
 
         return $total;
