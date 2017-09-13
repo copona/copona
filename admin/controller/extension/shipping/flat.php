0bb28025e264c9048f4a5e841c4153cc3adfa1d1
ArnisJ <arnis.juraga@gmail.com>
2017-09-13 17:07:36 +0300

    install/uninstall methods in admin/extension/shipping

--- admin/controller/extension/shipping/flat.php
+++ admin/controller/extension/shipping/flat.php
@@ -115,4 +115,14 @@ class ControllerExtensionShippingFlat extends Controller {
         return !$this->error;
     }
 
+    public function install() {
+        // bulk OC3 design install function
+        return 0;
+    }
+
+    public function uninstall() {
+        // bulk OC3 design install function
+        return 0;
+    }
+
 }
