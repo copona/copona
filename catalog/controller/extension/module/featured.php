0a669e590d4d32277294f6b1693de5ce26f5f1c7
ArnisJ <arnis.juraga@gmail.com>
2017-10-18 05:14:34 +0300

    Fix for Featured module MINIMUM add to cart

--- catalog/controller/extension/module/featured.php
+++ catalog/controller/extension/module/featured.php
@@ -67,6 +67,7 @@ class ControllerExtensionModuleFeatured extends Controller {
 
                     $data['products'][] = array(
                         'product_id'  => $product_info['product_id'],
+                        'minimum'     => $product_info['minimum'] == 0 ? 1 : $product_info['minimum'] ,
                         'thumb'       => $image,
                         'name'        => $product_info['name'],
                         'description' => utf8_substr(strip_tags(html_entity_decode($product_info['description'], ENT_QUOTES, 'UTF-8')), 0, $this->config->get($this->config->get('config_theme') . '_product_description_length')) . '..',
