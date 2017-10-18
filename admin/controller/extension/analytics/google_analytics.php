2a7f20b6e4a3cf24f67c49e9d8aea603575d1abf
arnisj <arnis.juraga@gmail.com>
2017-10-18 18:52:37 +0300

    analytics method install uninstall

--- admin/controller/extension/analytics/google_analytics.php
+++ admin/controller/extension/analytics/google_analytics.php
@@ -96,4 +96,13 @@ class ControllerExtensionAnalyticsGoogleAnalytics extends Controller {
         return !$this->error;
     }
 
+    public function install() {
+        // bulk OC3 design install function
+        return 0;
+    }
+
+    public function uninstall() {
+        // bulk OC3 design install function
+        return 0;
+    }
 }
