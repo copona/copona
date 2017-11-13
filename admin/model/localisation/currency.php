2b3f93c1e268ef2c443cefc01222f5a59b745f97
Janis Travins <janis.travins@indeed.pro>
2017-11-13 18:17:21 +0200

    fix 1a1d676 for Copona constants and folder creation if not exists

--- admin/model/localisation/currency.php
+++ admin/model/localisation/currency.php
@@ -134,14 +134,19 @@ class ModelLocalisationCurrency extends Model
 	    
         $zip = new \ZipArchive();
         $ecb_source_url = 'http://www.ecb.europa.eu/stats/eurofxref/eurofxref.zip';
-        $path_to_zip = DIR_STORAGE . 'download/ecb-data.zip';
-        $path_to_csv = DIR_STORAGE . 'download/eurofxref.csv';
+
+        if(!file_exists(DIR_STORAGE_PRIVATE . 'download')) {
+            mkdir( DIR_STORAGE_PRIVATE . 'download' );
+        };
+
+        $path_to_zip = DIR_STORAGE_PRIVATE . 'download/ecb-data.zip';
+        $path_to_csv = DIR_STORAGE_PRIVATE . 'download/eurofxref.csv';
         $compressed = file_get_contents($ecb_source_url);
         file_put_contents($path_to_zip, $compressed);
         $res = $zip->open($path_to_zip);
         // Make sure that we downloaded a valid zip archive
         if($res === TRUE) {
-            $zip->extractTo(DIR_STORAGE . 'download/');
+            $zip->extractTo(DIR_STORAGE_PRIVATE . 'download/');
             $zip->close();
             $data = file_get_contents($path_to_csv);
             $explode = explode(PHP_EOL, $data);
