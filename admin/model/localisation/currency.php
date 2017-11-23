0bab7ae1d7e69ba378b06515495a6b6140610432
arnisjuraga <arnis.juraga@gmail.com>
2017-11-23 14:55:30 +0200

    Fixed https://github.com/opencart/opencart/pull/6216

--- admin/model/localisation/currency.php
+++ admin/model/localisation/currency.php
@@ -4,9 +4,17 @@ class ModelLocalisationCurrency extends Model
 
     public function addCurrency($data)
     {
-        $this->db->query("INSERT INTO " . DB_PREFIX . "currency SET title = '" . $this->db->escape($data['title']) . "', code = '" . $this->db->escape($data['code']) . "', symbol_left = '" . $this->db->escape($data['symbol_left']) . "', symbol_right = '" . $this->db->escape($data['symbol_right']) . "', decimal_place = '" . $this->db->escape($data['decimal_place']) . "', value = '" . $this->db->escape($data['value']) . "', status = '" . (int)$data['status'] . "', date_modified = NOW()");
+        $this->db->query("INSERT INTO " . DB_PREFIX . "currency SET 
+        title = '" . $this->db->escape($data['title']) . "'
+        , code = '" . $this->db->escape($data['code']) . "'
+        , symbol_left = '" . $this->db->escape($data['symbol_left']) . "'
+        , symbol_right = '" . $this->db->escape($data['symbol_right']) . "'
+        , decimal_place = '" . $this->db->escape($data['decimal_place']) . "'        
+        , value = '" . (float)str_replace(",", ".", $data['value']) . "'
+        , status = '" . (int)$data['status'] . "', date_modified = NOW()");
 
         $currency_id = $this->db->getLastId();
+
 
         if ($this->config->get('config_currency_auto')) {
             $this->refresh(true);
@@ -19,15 +27,16 @@ class ModelLocalisationCurrency extends Model
 
     public function editCurrency($currency_id, $data)
     {
-        $this->db->query("UPDATE " . DB_PREFIX . "currency SET title = '" . $this->db->escape($data['title']) . "'"
-          . ", code = '" . $this->db->escape($data['code']) . "'"
-          . ", symbol_left = '" . $this->db->escape($data['symbol_left']) . "'"
-          . ", symbol_right = '" . $this->db->escape($data['symbol_right']) . "'"
-          . ", decimal_place = '" . $this->db->escape($data['decimal_place']) . "'"
-          . ", value = '" . $this->db->escape($data['value']) . "'"
-          . ", status = '" . (int)$data['status'] . "'"
-          . ", date_modified = NOW() "
-          . "WHERE currency_id = '" . (int)$currency_id . "'");
+        $this->db->query("UPDATE " . DB_PREFIX . "currency SET 
+        title = '" . $this->db->escape($data['title']) . "'
+        , code = '" . $this->db->escape($data['code']) . "'
+        , symbol_left = '" . $this->db->escape($data['symbol_left']) . "'
+        , symbol_right = '" . $this->db->escape($data['symbol_right']) . "'
+        , decimal_place = '" . $this->db->escape($data['decimal_place']) . "'
+        , value = '" . (float)str_replace(",", ".", $data['value']) . "'
+        , status = '" . (int)$data['status'] . "'
+        , date_modified = NOW()
+        WHERE currency_id = '" . (int)$currency_id . "'");
 
         $this->cache->delete('currency');
     }
