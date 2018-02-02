a85eab7e75d09d4716b5aa6d7707347cff234444
arnisj <arnis.juraga@gmail.com>
2018-02-02 10:53:37 +0200

    Fixed error in image resize name

--- catalog/controller/checkout/cart.php
+++ catalog/controller/checkout/cart.php
@@ -74,7 +74,7 @@ class ControllerCheckoutCart extends Controller {
                 }
 
                 if ($product['image']) {
-                     $image = $this->model_tool_image->{Config::get('theme_default_image_cart_resize','resize')}($product['image'], $this->config->get($this->config->get('config_theme') . '_image_cart_width'), $this->config->get($this->config->get('config_theme') . '_image_cart_height'));
+                     $image = $this->model_tool_image->{Config::get('theme_default_product_cart_thumb_resize','resize')}($product['image'], $this->config->get($this->config->get('config_theme') . '_image_cart_width'), $this->config->get($this->config->get('config_theme') . '_image_cart_height'));
                 } else {
                     $image = '';
                 }
