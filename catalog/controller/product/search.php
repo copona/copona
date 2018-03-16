ffb2de0333f642a1108c50a2ea5b2f3286fe2556
arnisjuraga <arnis.juraga@gmail.com>
2018-03-16 19:46:49 +0200

    Hook in search/index/after

--- catalog/controller/product/search.php
+++ catalog/controller/product/search.php
@@ -489,7 +489,7 @@ class ControllerProductSearch extends Controller {
         $data['content_bottom'] = $this->load->controller('common/content_bottom');
         $data['footer'] = $this->load->controller('common/footer');
         $data['header'] = $this->load->controller('common/header');
-
+        $this->hook->getHook('search/index/after', $data);
         $this->response->setOutput($this->load->view('product/search', $data));
     }
 
