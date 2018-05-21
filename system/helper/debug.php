0d47f9abcdd823ffcdc4b9430a533b129a655fa1
ArnisJ <arnis.juraga@gmail.com>
2018-05-21 16:07:28 +0000

    debug dt()

--- system/helper/debug.php
+++ system/helper/debug.php
@@ -111,11 +111,12 @@ if (!function_exists('dt')) {
 
     function dt()
     {
-        if (@$GLOBALS['debug_mode']) {
+        return loadTime::diff(); 
+        /*if (@$GLOBALS['debug_mode']) {
             !isset($_SESSION['dt_start_time']) ? $_SESSION['dt_start_time'] = microtime(true) : false;
             $diff = microtime(true) - $_SESSION['dt_start_time'];
             return $diff;
-        }
+        }*/
     }
 }
 
