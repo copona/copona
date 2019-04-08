4f3023b25469807eddceaa44901d03a4e54d48df
arnisjuraga <arnis.juraga@gmail.com>
2019-04-08 15:44:05 +0300

    function name conflict fix for __() foundation translator function :(

--- system/helper/translation.php
+++ system/helper/translation.php
@@ -1,7 +1,11 @@
 <?php
 
 
-
-function __($string, $domain = '') {
+/*
+ * TODO: deprecated! Should be possible to Use Laravel foundation helper, found in:
+ * vendor/laravel/framework/src/Illuminate/Foundation/helpers.php:937
+ * renamed for now.
+ * */
+function __copona($string, $domain = '') {
 	return Translation::get($string);
 }
