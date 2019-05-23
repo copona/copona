494ad5bd8cf77b67945127b18c73adfa09b075f8
arnisjuraga <arnis.juraga@gmail.com>
2019-05-23 15:43:59 +0300

    instal/uninstall

--- admin/controller/extension/module/latest.php
+++ admin/controller/extension/module/latest.php
@@ -165,4 +165,14 @@ class ControllerExtensionModuleLatest extends Controller {
         return !$this->error;
     }
 
+    public function install() {
+        // bulk OC3 design install function
+        return 0;
+    }
+
+    public function uninstall() {
+        // bulk OC3 design install function
+        return 0;
+    }
+
 }
