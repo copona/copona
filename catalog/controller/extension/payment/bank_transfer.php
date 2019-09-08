4c4406bb167dab81a7747799f9c2e44e5bd79624
arnisjuraga <arnis.juraga@gmail.com>
2019-09-08 04:07:54 +0300

    Bank Transfer some output for debug

--- catalog/controller/extension/payment/bank_transfer.php
+++ catalog/controller/extension/payment/bank_transfer.php
@@ -1,41 +1,43 @@
 <?php
 class ControllerExtensionPaymentBankTransfer extends Controller {
+	public function index() {
+		$this->load->language('extension/payment/bank_transfer');
 
-    public function index() {
-        $data = $this->load->language('extension/payment/bank_transfer');
+		$data['text_instruction'] = $this->language->get('text_instruction');
+		$data['text_description'] = $this->language->get('text_description');
+		$data['text_payment'] = $this->language->get('text_payment');
+		$data['text_loading'] = $this->language->get('text_loading');
 
-        $data['text_instruction'] = $this->language->get('text_instruction');
-        $data['text_description'] = $this->language->get('text_description');
-        $data['text_payment'] = $this->language->get('text_payment');
-        $data['text_loading'] = $this->language->get('text_loading');
+		$data['button_confirm'] = $this->language->get('button_confirm');
 
-        $data['button_confirm'] = $this->language->get('button_confirm');
+		$data['bank'] = nl2br($this->config->get('bank_transfer_bank' . $this->config->get('config_language_id')));
 
-        $data['bank'] = html_entity_decode( $this->config->get('bank_transfer_bank' . $this->config->get('config_language_id')) );
+		$data['continue'] = $this->url->link('checkout/success');
 
-        $data['continue'] = $this->url->link('checkout/success');
-        $data['back'] = $this->url->link('checkout/checkout');
+		return $this->load->view('extension/payment/bank_transfer', $data);
+	}
 
+	public function confirm() {
 
+	    $json = [];
 
+		if ($this->session->data['payment_method']['code'] == 'bank_transfer') {
+			$this->load->language('extension/payment/bank_transfer');
 
-        return $this->load->view('extension/payment/bank_transfer', $data);
-    }
+			$this->load->model('checkout/order');
 
-    public function confirm() {
-        if (isset($this->session->data['payment_method']['code']) && $this->session->data['payment_method']['code'] == 'bank_transfer') {
-            $this->load->language('extension/payment/bank_transfer');
+			$comment  = $this->language->get('text_instruction') . "\n\n";
+			$comment .= $this->config->get('bank_transfer_bank' . $this->config->get('config_language_id')) . "\n\n";
+			$comment .= $this->language->get('text_payment');
 
-            $this->load->model('checkout/order');
+			$this->model_checkout_order->addOrderHistory($this->session->data['order_id'], $this->config->get('bank_transfer_order_status_id'), $comment, true);
 
-            $comment = $this->language->get('text_instruction') . "\n\n";
-            $comment .= $this->config->get('bank_transfer_bank' . $this->config->get('config_language_id')) . "\n\n";
-            $comment .= $this->language->get('text_payment');
+            $json['message'] = 'Order created!';
+            $json['order_id'] = $this->session->data['order_id'];
 
-            $comment = html_to_plaintext($comment, true);
+            $this->response->addHeader('Content-Type: application/json');
+            $this->response->setOutput(json_encode($json));
 
-            $this->model_checkout_order->addOrderHistory($this->session->data['order_id'], $this->config->get('bank_transfer_order_status_id'), $comment, true);
-        }
-    }
-
+		}
+	}
 }
